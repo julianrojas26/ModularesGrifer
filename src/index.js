c63b55b1a0d9e26d721fb14026a14cbddd812bfb
julian <julirojastoque@gmail.com>
2023-03-11 17:24:49 -0500

    backendlogin

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import indexRoutes from "./routes/routes.js";
-import conection from "../db/db.js";
 
 const app = express();
 
@@ -16,10 +15,3 @@ app.use(express.static(join(__dirname, "/public")));
 
 app.listen(3000);
 console.log("El servidor se esta ejecutando en el puerto localhost:3000");
-
-export function ingreso(user, pass) {
-  conection.query("select * from administrador", (err, res) => {
-    if (err) throw err;
-  });
-  conection.end()
-}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,32 +1,57 @@
 import { Router } from "express";
-import bodyParser from "body-parser";
+import multer from "multer";
+import conection from "../../db/db.js";
 
 const router = Router();
 
-router.use( bodyParser.urlencoded({ extended: true }) );
-router.use( bodyParser.json() );
+export function ingreso(user, pass, route) {
+  conection.query("select * from administrador", (err, res) => {
+    if (err) throw err;
+    let i = 0;
+    let acc = false;
+    res.forEach((u) => {
+      if (u.usuario == user && u.contraseña == pass) {
+       acc = true
+      }
+    });
+    if (acc == true) {
+      route.render("estadisticas")
+    } else {
+      route.send("Usuario incorrecto")
+    }
+  });
+}
 
-router.get("/", (req, res) => {
-    res.render("index")
+const upload = multer({
+  dest: "src/uploads",
 });
 
-router.get("/nosotros", (req,res) => {
-    res.render("nosotros")
-})
+router.get("/", (req, res) => {
+  res.render("index");
+});
 
-router.get("/admin", (req, res) => {
-    res.render("admin")
-}).post("/a", (req, res) => {
-    console.log(req)
-    res.send("hola")
-})
+router.get("/nosotros", (req, res) => {
+  res.render("nosotros");
+});
+
+const file = upload.single("file");
+
+router
+  .get("/admin", (req, res) => {
+    res.render("admin");
+  })
+  .post("/login", file, (req, res) => {
+    const data = req.body;
+    console.log(data.user + data.pass);
+    ingreso(data.user, data.pass, res);
+  });
 
 router.get("/portafolio", (req, res) => {
-    res.render("portafolio")
-})
+  res.render("portafolio");
+});
 
 router.get("/contacto", (req, res) => {
-    res.render("contacto")
-})
+  res.render("contacto");
+});
 
 export default router;
