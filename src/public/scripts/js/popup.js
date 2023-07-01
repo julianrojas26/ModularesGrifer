d95744728edba31ba929a075b04eb3aa1e419ce3
Yera <andresbarey165@gmail.com>
2023-07-01 13:20:27 -0500

    0.1

---
+++ src/public/scripts/js/popup.js
@@ -0,0 +1,101 @@
+/* Cocinas */
+
+var open_coc1 = document.getElementById('open_coc1'),
+    cocina1 = document.getElementById('cocina1'),
+    popup_cocina1 = document.getElementById('popup_cocina1'),
+    btn_cerrar_popup = document.getElementById('btn_cerrar_popup');
+
+open_coc1.addEventListener('click',function(){
+    cocina1.classList.add('active');
+});
+btn_cerrar_popup.addEventListener('click',function(){
+    cocina1.classList.remove('active');
+});
+/* ------------------------------------------------------------------------------------------- */
+var open_coc2 = document.getElementById('open_coc2'),
+    cocina2 = document.getElementById('cocina2'),
+    popup_cocina2 = document.getElementById('popup_cocina2'),
+    btn_cerrar_popup2 = document.getElementById('btn_cerrar_popup2');
+
+open_coc2.addEventListener('click',function(){
+    cocina2.classList.add('active');
+});
+btn_cerrar_popup2.addEventListener('click',function(){
+    cocina2.classList.remove('active');
+});
+/* ------------------------------------------------------------------------------------------- */
+var open_coc3 = document.getElementById('open_coc3'),
+    cocina3 = document.getElementById('cocina3'),
+    popup_cocina3 = document.getElementById('popup_cocina3'),
+    btn_cerrar_popup3 = document.getElementById('btn_cerrar_popup3');
+
+open_coc3.addEventListener('click',function(){
+    cocina3.classList.add('active');
+});
+btn_cerrar_popup3.addEventListener('click',function(){
+    cocina3.classList.remove('active');
+});
+/* ------------------------------------------------------------------------------------------- */
+var open_coc4 = document.getElementById('open_coc4'),
+    cocina4 = document.getElementById('cocina4'),
+    popup_cocina4 = document.getElementById('popup_cocina4'),
+    btn_cerrar_popup4 = document.getElementById('btn_cerrar_popup4');
+
+open_coc4.addEventListener('click',function(){
+    cocina4.classList.add('active');
+});
+btn_cerrar_popup4.addEventListener('click',function(){
+    cocina4.classList.remove('active');
+});
+/* ------------------------------------------------------------------------------------------- */
+
+/* Closets */
+
+var ope_clo1 = document.getElementById('ope_clo1'),
+    closet1 = document.getElementById('closet1'),
+    popup_closet1 = document.getElementById('popup_closet1'),
+    btn_cerrar_popup_clo1 = document.getElementById('btn_cerrar_popup_clo1');
+
+ope_clo1.addEventListener('click',function(){
+    closet1.classList.add('active');
+});
+btn_cerrar_popup_clo1.addEventListener('click',function(){
+    closet1.classList.remove('active');
+});
+/* ------------------------------------------------------------------------------------------- */
+var ope_clo2 = document.getElementById('ope_clo2'),
+    closet2 = document.getElementById('closet2'),
+    popup_closet2 = document.getElementById('popup_closet2'),
+    btn_cerrar_popup_clo2 = document.getElementById('btn_cerrar_popup_clo2');
+
+ope_clo2.addEventListener('click',function(){
+    closet2.classList.add('active');
+});
+btn_cerrar_popup_clo2.addEventListener('click',function(){
+    closet2.classList.remove('active');
+});
+/* ------------------------------------------------------------------------------------------- */
+var ope_clo3 = document.getElementById('ope_clo3'),
+    closet3 = document.getElementById('closet3'),
+    popup_closet3 = document.getElementById('popup_closet3'),
+    btn_cerrar_popup_clo3 = document.getElementById('btn_cerrar_popup_clo3');
+
+ope_clo3.addEventListener('click',function(){
+    closet3.classList.add('active');
+});
+btn_cerrar_popup_clo3.addEventListener('click',function(){
+    closet3.classList.remove('active');
+});
+/* ------------------------------------------------------------------------------------------- */
+var ope_clo4 = document.getElementById('ope_clo4'),
+    closet4 = document.getElementById('closet4'),
+    popup_closet4 = document.getElementById('popup_closet4'),
+    btn_cerrar_popup_clo4 = document.getElementById('btn_cerrar_popup_clo4');
+
+ope_clo4.addEventListener('click',function(){
+    closet4.classList.add('active');
+});
+btn_cerrar_popup_clo4.addEventListener('click',function(){
+    closet4.classList.remove('active');
+});
+/* ------------------------------------------------------------------------------------------- */
