21c5417d1b94079f70ec467c51f7ce6c6de710df
julian <julirojastoque@gmail.com>
2023-07-01 16:11:48 -0500

    server

---
+++ src/public/scripts/popup.js
@@ -0,0 +1,49 @@
+/* Cocinas */
+
+var open_coc1 = document.getElementById('open_coc1'),
+    cocina1 = document.getElementById('cocina1'),
+    popup_cocina1 = document.getElementById('popup_cocina1'),
+    btn_cerrar_popup = document.getElementById('btn_cerrar_popup');
+
+open_coc1.addEventListener('click',function(){
+    cocina1.classList.add('active');
+});
+btn_cerrar_popup.addEventListener('click',function(){
+    cocina1.classList.remove('active');
+});
+/* ------------------------------------------------------------------------------------------- */
+var open_coc2 = document.getElementById('open_coc2'),
+    cocina2 = document.getElementById('cocina2'),
+    popup_cocina2 = document.getElementById('popup_cocina2'),
+    btn_cerrar_popup2 = document.getElementById('btn_cerrar_popup2');
+
+open_coc2.addEventListener('click',function(){
+    cocina2.classList.add('active');
+});
+btn_cerrar_popup2.addEventListener('click',function(){
+    cocina2.classList.remove('active');
+});
+/* ------------------------------------------------------------------------------------------- */
+var open_coc3 = document.getElementById('open_coc3'),
+    cocina3 = document.getElementById('cocina3'),
+    popup_cocina3 = document.getElementById('popup_cocina3'),
+    btn_cerrar_popup3 = document.getElementById('btn_cerrar_popup3');
+
+open_coc3.addEventListener('click',function(){
+    cocina3.classList.add('active');
+});
+btn_cerrar_popup3.addEventListener('click',function(){
+    cocina3.classList.remove('active');
+});
+/* ------------------------------------------------------------------------------------------- */
+var open_coc4 = document.getElementById('open_coc4'),
+    cocina4 = document.getElementById('cocina4'),
+    popup_cocina4 = document.getElementById('popup_cocina4'),
+    btn_cerrar_popup4 = document.getElementById('btn_cerrar_popup4');
+
+open_coc4.addEventListener('click',function(){
+    cocina4.classList.add('active');
+});
+btn_cerrar_popup4.addEventListener('click',function(){
+    cocina4.classList.remove('active');
+});
